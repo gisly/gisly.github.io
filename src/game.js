74ff87d0892aba28b8407bbde65df106d573794c
unknown <gisly@list.ru>
2013-09-12 22:06:33 +0400

    changed format

--- src/game.js
+++ src/game.js
@@ -32,7 +32,8 @@ Game = {
     // Initialize and start our game
     start: function() {
         // Start crafty and set a background color so that we can see it's working
-        Crafty.init(Game.width(), Game.height());
+        //Crafty.init(Game.width(), Game.height());
+        Crafty.init();
         //Crafty.background('rgb(87, 109, 20)');
 
         // Simply start the "Loading" scene to get things going
