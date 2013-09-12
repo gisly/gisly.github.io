bee1f1e83ac4401c89355c67a1fd55877b674ba6
unknown <gisly@list.ru>
2013-09-12 21:29:37 +0400

    changed format

--- src/scenes.js
+++ src/scenes.js
@@ -169,7 +169,7 @@ function createMemoryArray(){
 }
 
 function createCardByIndex(curCardName, colIndex, rowIndex){
-	return Crafty.e('2D, DOM, Mouse, '+curCardName)
+	return Crafty.e(curCardName+', 2D, DOM, Mouse')
                 .setName(curCardName)
                 .attr({x: colIndex*(CARD_WIDTH + PADDING_H) + OFFSET, 
                 	y: rowIndex*(CARD_HEIGHT + PADDING_V)
@@ -222,8 +222,7 @@ Crafty.scene('MainMenu', function() {
 	];
 	
 	displayGameButtonArray(titleArray);
-
-
+
 });
 
 
